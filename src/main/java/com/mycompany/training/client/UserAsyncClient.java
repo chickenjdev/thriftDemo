2b378afa6ca3bf669143559aa6af88515f7eb75a
khoinht <khoinht@vng.com.vn>
2022-05-24 18:13:56 +0700

    threadpool server

--- src/main/java/com/mycompany/training/client/UserAsyncClient.java
+++ src/main/java/com/mycompany/training/client/UserAsyncClient.java
@@ -15,11 +15,12 @@ import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.layered.TFramedTransport;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class UserAsyncClient {
-    public UserAsyncClient() throws TException, IOException {
+    public UserAsyncClient(String sessionId) throws TException, IOException {
 
-        TSocket socket = new TSocket("localhost", 3031,30000);
+        TSocket socket = new TSocket("localhost", 3031, 30000);
         TTransport framedTransport = new TFramedTransport(socket);
         framedTransport.open();
         TProtocol protocol = new TBinaryProtocol(framedTransport);
@@ -27,7 +28,7 @@ public class UserAsyncClient {
         UserManager.Client client = new UserManager.Client(protocol);
         new Thread(() -> {
             try {
-                ResponseData result = client.getUserBySession("1334eaa5-9679-4d0e-8b81-6d36895ec774");
+                ResponseData result = client.getUserBySession(sessionId);
                 System.out.println("get userBySessionID : " + result.toString());
             } catch (TException e) {
                 throw new RuntimeException(e);
@@ -36,9 +37,17 @@ public class UserAsyncClient {
     }
 
     public static void main(String[] args) throws TException, IOException {
-        new UserAsyncClient();
-        new UserAsyncClient1();
-        new UserAsyncClient();
-        new UserAsyncClient1();
+        String[] arr = new String[]{
+                "1334eaa5-9679-4d0e-8b81-6d36895ec774",
+                "4802c84c-240f-4352-9b62-7b557489ce8a",
+                "1334eaa5-9679-4d0e-8b81-6d36895ec774",
+                "4802c84c-240f-4352-9b62-7b557489ce8a",
+                "1334eaa5-9679-4d0e-8b81-6d36895ec774",
+                "4802c84c-240f-4352-9b62-7b557489ce8a",
+                "1334eaa5-9679-4d0e-8b81-6d36895ec774"};
+        for (int i = 0; i < arr.length; i++) {
+            new UserAsyncClient(arr[i]);
+        }
+
     }
 }
